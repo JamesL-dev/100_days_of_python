2b58e485e830aca9a0e3d4c1fb32a271fdd08e08
JamesL-dev <lass2922@vandals.uidaho.edu>
2022-10-26 00:45:26 -0700

    day1

---
+++ day_1/main.py
@@ -0,0 +1,8 @@
+# Day 1 of 100 days of python
+# This program comes up with a not so great band name
+
+print("Welcome to the Band Name Generator.\n")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + city_name + " " + pet_name)
+
