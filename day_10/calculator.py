2383afff56b40f01adebee2e2664081fc0bfdd21
JamesL-dev <lass2922@vandals.uidaho.edu>
2022-11-08 01:05:14 -0800

    classic calculator program

---
+++ day_10/calculator.py
@@ -0,0 +1,49 @@
+# Classic Calc app
+from art import logo
+
+
+# Add
+def add(n1, n2):
+    total = n1 + n2
+    return total
+# Subtract
+def subtract(n1, n2):
+    total = n1 - n2
+    return total
+# Multiply
+def multiply(n1, n2):
+    total = n1 * n2
+    return total
+# Divide
+def divide(n1, n2):
+    total = n1 /+ n2
+    return total
+
+operations = {
+        "+": add,
+        "-": subtract,
+        "*": multiply,
+        "/": divide
+}
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to coninue calculating with {answer}, or type 'n' to start a new calculation.: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
