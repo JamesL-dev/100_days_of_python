ed0d88317ea8ecb43e86ca16d4b61b6ad7dd57a8
JamesL-dev <lass2922@vandals.uidaho.edu>
2022-12-05 23:56:39 -0800

    add buttons and title

--- day_28/main.py
+++ day_28/main.py
@@ -20,12 +20,25 @@ window = tkinter.Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+
+title_label = tkinter.Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
+
 canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tkinter.PhotoImage(file="./tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
+start_button = tkinter.Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = tkinter.Button(text="reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_marks = tkinter.Label(text="✔", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
 
 window.mainloop()
